c18266324c22c1eec97d1c8a2f2ec3c070ac7c0c
Meissner Stephan (DCCS) <smeissner@dccsintra.net>
2019-11-14 12:54:42 +0100

    removed cache from git.

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('C:/Users/smeissner.DCCSINTRA/OneDrive/dev/tischlerzthening2018/v3a/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
-      options: {"plugins":[],"pathToConfigModule":"src/utils/typography.js"},
-    },{
-      plugin: require('C:/Users/smeissner.DCCSINTRA/OneDrive/dev/tischlerzthening2018/v3a/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
